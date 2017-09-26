f28f364415aad10f0b0a34ce0f09a2256b3d885c
Dener Carmo <dener.wilian@gmail.com>
2017-09-26 14:59:48 -0400

    Adding cg

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def download_curriculum
+    send_file(
+      "#{Rails.root}/public/cv-dener.pdf",
+      filename: "cv-dener.pdf",
+      type: "application/pdf"
+    )
+
+    render nothing: true
+  end
 end
